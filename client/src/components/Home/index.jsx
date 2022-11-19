0d654e2279d08021b6810bdae8ff344c701915d7
mahesh3536 <maheshjivarani3536@gmail.com>
2022-11-19 14:08:56 +0530

    added home directory

---
+++ client/src/components/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './styles.module.css'
+function Home() {
+    return (
+       <div className={styles.container}>
+           <div className={styles.container1}>
+            welcome to our community 
+           </div>
+       </div>
+    )
+}
+export default Home
