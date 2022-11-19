4723c9f1f4ca455dda711d3b61268b38d6d73ff5
mahesh3536 <maheshjivarani3536@gmail.com>
2022-11-19 14:56:27 +0530

    login and logout

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
 import styles from './styles.module.css'
+import { useState, useEffect } from 'react';
+import Login from '../Login/index';
+import {gapi} from "gapi-script"
+import Logout from '../Logout';
+
+const clientId = "802182116275-0u77gmcntf7j70s9snpkjtqlpiucfvrc.apps.googleusercontent.com"
+
 function Home() {
+    const [userName, setUserName] = useState();
+
+  useEffect(() => {
+    const start = () => {
+      gapi.auth2.init({
+        clientId: clientId,
+        scope: ""
+      })
+    }
+
+    gapi.load('client:auth2', start)
+  })
     return (
        <div className={styles.container}>
            <div className={styles.container1}>
-            welcome to our community 
+            <div className={styles.container2}>Welcome to our community</div> 
+            <div className={styles.container3}>#Social App</div>
+            {userName ?
+        <Logout/>
+        :
+        <Login />
+      }
+
            </div>
+           
        </div>
     )
 }
