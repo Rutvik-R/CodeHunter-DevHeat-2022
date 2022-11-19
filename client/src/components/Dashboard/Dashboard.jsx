e16879597e0ac376af80088096e5d34a5fabcca6
kaushik2107-bit <kaushiksbhowmick@gmail.com>
2022-11-19 16:25:38 +0530

    userDashboard

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,16 @@
 import styles from "./styles.module.css"
+import Chat from "./dashboardComponents/Chats/Chat"
+import OnlineNow from "./dashboardComponents/OnlineNow/OnlineNow"
+import Selectchats from "./dashboardComponents/SelectChats/SelectChats"
 
 export default function Dashboard() {
 	return (
-		<div className={styles.user_dashboard}> 
-			
+		<div className={styles.user__dashboard}> 
+			<Chat />
+			<div className={styles.select__chats}>
+				<OnlineNow />
+				<Selectchats />
+			</div>
 		</div>
 	)
 } 
